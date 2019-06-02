c1469aa8e600bf210eac3a17aae73a9a42932766
debadyuti <debadyuti4@gmail.com>
2019-06-02 07:48:03 +0530

    Added newline

--- pi/vuln_assess.py
+++ pi/vuln_assess.py
@@ -20,6 +20,6 @@ if __name__ == "__main__":
             for port in ports:
                 services = services + str(port["service"]) + ", "
                 open_ports = open_ports + str(port["port"].split("/")[0]) + ", " 
-            message = message + "In device with IP: " + filename.split(".json")[0] + " services " + services + "\b\b are running on ports " + open_ports + "\b\b respectively and may be vulnerable to attacks.\n"  
+            message = message + "In device with IP: " + filename.split(".json")[0] + " services " + services + "\b\b are running on ports " + open_ports + "\b\b respectively and may be vulnerable to attacks.\n\n"  
     send_message(message)
     shutil.rmtree(path)
